30e5b16b1dde0c4020e7cc476f4366e81623b268
Sharanyanaik98 <sharanyakul98@gmail.com>
2026-08-03 14:00:32 +0000

    new file added

---
+++ scripts/clean_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Remove existing application files safely
+rm -rf /usr/share/nginx/html/*
+
---
+++ scripts/restart_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Restart server to ensure zero-downtime cache clearing
+sudo systemctl restart nginx
+
